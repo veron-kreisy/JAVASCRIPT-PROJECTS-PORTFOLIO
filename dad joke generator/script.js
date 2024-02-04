41153fdc7607c56b0d900039fbdf004878439d7c
miss_codein <mbula0563@gmail.com>
2024-02-04 21:59:46 +0300

    Update script.js

--- dad joke generator/script.js
+++ dad joke generator/script.js
@@ -1,73 +1,32 @@
 // calling APIs in javascript
 
 
-// Define the API URL
-const apiUrl = 'https://icanhazdadjoke.com/';
-const outputElement = document.getElementById('output');
 
-// Make a GET request
-fetch(apiUrl)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    outputElement.textContent = JSON.stringify(data, null, 2);
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+// chat gpt version
 
-// stackoverflow
-
-async function dummy() {
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "text/plain");
-  
-    var requestOptions = {
-      method: "POST",
-      headers: myHeaders,
-      body: JSON.stringify({}),
-      redirect: "follow",
-    };
-  
-    return fetch("some.api.com", requestOptions);
-  }
-  
-  document.getElementById("button1").onclick = async function () {
-    var res = await dummy();
-    return res.ok;
-  };
-//   example 2
-function dummy() {
-
-    return new Promise((resolve, reject) => {
-      var myHeaders = new Headers();
-            myHeaders.append("Content-Type", "application/json");
-
-        var requestOptions = {
-          method: "GET",
-          headers: myHeaders,
-          body: JSON.stringify({}),
-        };
-        
-        
-        fetch('https://jsonplaceholder.typicode.com/todos/1')
+document.addEventListener("DOMContentLoaded", function () {
+    // Function to fetch a random dad joke from the API
+    function fetchDadJoke() {
+        fetch("https://icanhazdadjoke.com/", {
+            headers: {
+                "Accept": "application/json"
+            }
+        })
         .then(response => response.json())
         .then(data => {
-          resolve(data);
+            // Display the joke in the #joke paragraph
+            document.getElementById("joke").innerText = data.joke;
         })
-        .catch((error) => {
-          reject(error);
+        .catch(error => {
+            console.log("Error fetching dad joke:", error);
         });
-    })
-}
-const main = async()=>{
-console.log(await dummy());
-console.log("foo bar")
-} 
+    }
 
-main();
+    // Preload a dad joke when the page loads
+    fetchDadJoke();
+
+    // Event listener for the "Get Another Joke" button
+    document.getElementById("get-joke-btn").addEventListener("click", function () {
+        fetchDadJoke();
+    });
+});
