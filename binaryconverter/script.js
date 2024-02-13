bc8a0cbb5689af437df91bcd0bdde85221a1364e
miss_codein <mbula0563@gmail.com>
2024-02-13 23:15:49 +0300

    Update script.js

--- binaryconverter/script.js
+++ binaryconverter/script.js
@@ -1,3 +1,11 @@
  const numberInput=document.getElementById('number-input');
 const convertBtn=document.getElementById('convert-btn');
 const result=document.getElementById('result');
+
+// checking the value of a number
+
+function checkUserInput(){
+
+    console.log(numberInput.value);
+
+}
