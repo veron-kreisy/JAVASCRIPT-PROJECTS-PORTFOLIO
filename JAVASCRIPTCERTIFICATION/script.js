97173f53c4a4fe5779d95671e0c1ffad93177fe8
miss_codein <mbula0563@gmail.com>
2024-06-17 21:30:27 +0300

    Update script.js

--- JAVASCRIPTCERTIFICATION/script.js
+++ JAVASCRIPTCERTIFICATION/script.js
@@ -1,6 +1,4 @@
 
-
-let character ='Hello'
 // when reassigning variables don't use the let keyword
 // camel case is when a variable is more than one word,so you capitalise the otherword 
  let secondCharacter="Test";
