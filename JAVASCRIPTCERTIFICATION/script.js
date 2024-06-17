84fa1e1232843c2c895d9877f9f7e1955b746a1c
miss_codein <mbula0563@gmail.com>
2024-06-17 13:37:33 +0300

    Update script.js

--- JAVASCRIPTCERTIFICATION/script.js
+++ JAVASCRIPTCERTIFICATION/script.js
@@ -3,4 +3,18 @@ console.log(character);
 // when reassigning variables don't use the let keyword
 // camel case is when a variable is more than one word,so you capitalise the otherword 
  let secondCharacter="Test";
- 
+//  trying to learn how to asisgn variables
+secondCharacter=character;
+// another learning instance
+let profession= "teacher";
+let age;
+console.log(profession);
+console.log(age);
+// count variable
+let count=8;
+// addition on console
+console.log(count+1);
+// arrays in programming
+let rows=["Naomi","Quincy","CamperChan"];
+console.log(rows[0]);
+
